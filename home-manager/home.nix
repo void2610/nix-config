de06380da95de84c6ce14d172049db5cf3a8d98b
void2610 <izumisan2610@gmail.com>
2026-03-31 17:51:49 +0900

    Move shell session environment into home-manager

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,9 +1,11 @@
 { pkgs, ... }:
-
+let
+  homeDir = "/Users/shuya";
+in
 {
   # ホームディレクトリとユーザー名
   home.username = "shuya";
-  home.homeDirectory = "/Users/shuya";
+  home.homeDirectory = homeDir;
 
   # home-manager のバージョン
   home.stateVersion = "24.11";
@@ -12,6 +14,17 @@
   home.packages = with pkgs; [
     dotnet-sdk # Unity プロジェクトのフォーマットチェックで使用
   ];
+
+  home.sessionPath = [
+    "/Applications/platform-tools"
+    "${homeDir}/Documents"
+    "${homeDir}/.local/bin"
+    "${homeDir}/.yarn/bin"
+  ];
+
+  home.sessionVariables = {
+    DOTNET_ROOT = "${pkgs.dotnet-sdk}/share/dotnet";
+  };
 
   # home-manager 自身による管理を有効化
   programs.home-manager.enable = true;
