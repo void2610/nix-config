75a3f75455e7c344287d448b76b7b9213c9510f3
void2610 <contact@void2610.dev>
2026-04-05 08:38:14 +0900

    Update security.nix

--- nix-darwin/modules/security.nix
+++ nix-darwin/modules/security.nix
@@ -1,3 +1,11 @@
 {
+  networking.applicationFirewall = {
+    enable = true;
+    blockAllIncoming = false;
+    allowSigned = true;
+    allowSignedApp = true;
+    enableStealthMode = false;
+  };
+
   security.pam.services.sudo_local.touchIdAuth = true;
 }
